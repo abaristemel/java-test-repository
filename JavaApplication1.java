cf8d528b300ebaea93e8043c0102db81ab62fd69
Ahmet Barış TEMEL <abaris.temel@outlook.com>
2022-05-04 14:39:28 +0300

    test commit

---
+++ JavaApplication1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author baris
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 10;
+        int y;
+        int baris;
+        baris = 10;
+        System.out.println("Merhaba Dunya" + baris);
+        baris = 20;
+        System.out.println("Merhaba Dunya" + baris);
+        baris = 100;
+        System.out.println("Merhaba Dunya" + baris);
+        double pi =  3.14;
+        String s = "benim ismim ahmet baris temel";
+        System.out.println( " dizgi " + s + " pi : " + pi);
+        //System.out.println(" pi : " + pi);
+        
+        
+
+        
+        //System.out.println("Ahmet Baris Temel");
+        // unutmamak için not alıyorum
+        /*
+        
+        deneme 
+        mesajı
+        
+        */
+    }
+    
+}
